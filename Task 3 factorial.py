8397d88871265e7cb0e2c6503cbb02492bcc43b4
swaminathanss <swami1203nathan@gmail.com>
2024-08-10 09:51:51 +0530

    Delete Task 3 factorial.py

--- Task 3 factorial.py
+++
@@ -1,7 +0,0 @@
-def factorial(n):
-    
-    
-    return 1 if (n==1 or n==0) else n * factorial(n - 1) 
-print('Enter the number')
-num = int(input())
-print("Factorial of",num,"is",factorial(num))
